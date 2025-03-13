c64b1835d7546e4d752a8971102d120b6f74dcee
Hary Capri <76942747+harycp@users.noreply.github.com>
2025-03-13 21:01:44 +0700

    feat: add models for tasks

---
+++ services/project-service/src/models/task.js
@@ -0,0 +1,53 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../database");
+
+const task = sequelize.define(
+  "task",
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    list_id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      references: {
+        model: "lists",
+        key: "id",
+      },
+    },
+    assignee_id: {
+      type: DataTypes.UUID,
+      allowNull: true,
+      references: {
+        model: "users",
+        key: "id",
+      },
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    due_date: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+    position: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        min: 1,
+      },
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = task;
