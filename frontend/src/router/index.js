030cfc465138216f3336ace01d2e99114595f06b
Hary Capri <76942747+harycp@users.noreply.github.com>
2025-03-14 14:25:26 +0700

    feat: initialize frontEnd folder with install vue-router, tailwind, etc.

---
+++ frontend/src/router/index.js
@@ -0,0 +1,17 @@
+import { createMemoryHistory, createRouter } from "vue-router";
+import HelloWorld from "../components/HelloWorld.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: HelloWorld,
+  },
+];
+
+const router = createRouter({
+  history: createMemoryHistory(),
+  routes,
+});
+
+export default router;
