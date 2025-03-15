79806f339c97fc0f1311be1fa0f9a8a42b8044f9
dihaayyy <148746377+dihaayyy@users.noreply.github.com>
2025-03-15 13:18:11 +0700

    feat: add navbar on landing page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,15 @@
-<!-- <script setup>
+<script setup>
+import { RouterView } from "vue-router";
+import Navbar from "./components/Navbar.vue";
 import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
   <div>
+    <Navbar />
+    <RouterView />
+  </div>
+  <!-- <div>
     <a href="https://vite.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
@@ -11,11 +17,11 @@ import HelloWorld from "./components/HelloWorld.vue";
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <HelloWorld msg="Vite + Vue" /> -->
 </template>
 
 <style scoped>
-.logo {
+/* .logo {
   height: 6em;
   padding: 1.5em;
   will-change: filter;
@@ -26,11 +32,6 @@ import HelloWorld from "./components/HelloWorld.vue";
 }
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style> -->
+} */
+</style>
 
-<template>
-  <main>
-    <RouterView />
-  </main>
-</template>
--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,3 +1,3 @@
 <template>
-  <h1 class="text-3xl font-bold text-center">Hello World</h1>
+  <h1 class="text-3xl font-bold text-center mt-4">Hello World</h1>
 </template>
