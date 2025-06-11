d2ff319becae7efd559addf3d244aeef081b9f50
Hary Capri <76942747+harycp@users.noreply.github.com>
2025-06-11 16:35:24 +0700

    feat: updateTask and updateTaskPosition

--- services/project-service/src/services/taskService.js
+++ services/project-service/src/services/taskService.js
@@ -19,4 +19,60 @@ const createTask = async (listId, taskData) => {
   return newTask;
 };
 
-module.exports = { createTask };
+const updateTask = async (taskId, updateData) => {
+  const task = await taskModel.findByPk(taskId);
+  if (!task) {
+    throw new Error("Task not found");
+  }
+  await task.update(updateData);
+
+  return task;
+};
+
+const updateTaskPosition = async (taskId, newPosition) => {
+  const t = await sequelize.transaction();
+  try {
+    const taskToMove = await taskModel.findByPk(taskId, { transaction: t });
+    if (!taskToMove) throw new Error("Task not found");
+
+    const oldPosition = taskToMove.position;
+    const listId = taskToMove.list_id;
+
+    if (oldPosition === newPosition) {
+      await t.commit();
+      return;
+    }
+
+    if (oldPosition < newPosition) {
+      await taskModel.update(
+        { position: sequelize.literal("position - 1") },
+        {
+          where: {
+            list_id: listId,
+            position: { [Op.gt]: oldPosition, [Op.lte]: newPosition },
+          },
+          transaction: t,
+        }
+      );
+    } else {
+      await taskModel.update(
+        { position: sequelize.literal("position + 1") },
+        {
+          where: {
+            list_id: listId,
+            position: { [Op.gte]: newPosition, [Op.lt]: oldPosition },
+          },
+          transaction: t,
+        }
+      );
+    }
+
+    await taskToMove.update({ position: newPosition }, { transaction: t });
+    await t.commit();
+  } catch (error) {
+    await t.rollback();
+    throw error;
+  }
+};
+
+module.exports = { createTask, updateTask, updateTaskPosition };
