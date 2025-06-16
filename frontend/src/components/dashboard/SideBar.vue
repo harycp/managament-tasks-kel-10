a6e6a0518c8cc2b83170d34eff7f83c1c660d505
Hary Capri <76942747+harycp@users.noreply.github.com>
2025-06-16 20:27:30 +0700

    FINISH V.2

--- frontend/src/components/dashboard/SideBar.vue
+++ frontend/src/components/dashboard/SideBar.vue
@@ -108,7 +108,7 @@ export default defineComponent({
     const logout = async () => {
       try {
         await axios.post(
-          "http://localhost/user-service/auth/logout",
+          "https://localhost/user-service/auth/logout",
           {},
           {
             withCredentials: true,
